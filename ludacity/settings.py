05c2fc5302c0535e337046c13a40d53e9fe5796e
DigitalSuricate <benoit.branchard@gmail.com>
2015-05-19 14:37:54 +0200

    database configuration

--- ludacity/settings.py
+++ ludacity/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'ludacity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': 'ludacity',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
