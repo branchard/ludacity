0ae7ff4911bb00e299d43ae8e6f5b3771c5f3b18
Benoit <benoit.branchard@gmail.com>
2015-05-20 23:07:18 +0200

    database updated

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from polymorphic import PolymorphicModel
 # Doc: http://www.apidev.fr/blog/2012/01/12/heritage-de-modele-avec-django/
 
 class User(PolymorphicModel):
-    username = models.CharField(max_length=255)
+    username = models.CharField(primary_key=True, max_length=255)
     lastname = models.CharField(max_length=255)
     firstname = models.CharField(max_length=255)
 
@@ -17,50 +17,58 @@ class Admin(User):
 
 
 class Teacher(User):
-    pass
-
-
-class Group(models.Model):
-    group_name = models.CharField(max_length=255)
+    groups = models.ManyToManyField('Group')
 
     def __str__(self):
-        return self.group_name
+        return "{0} ({1})".format(super(Teacher, self).__str__(), self.group)
 
 
 class Student(User):
-    group = models.ForeignKey(Group)
+    group = models.ForeignKey('Group')
 
     def __str__(self):
         return "{0} ({1})".format(super(Student, self).__str__(), self.group)
 
 
-class Exercise(models.Model):
-    exercise_name = models.CharField(max_length=255)
+class Group(models.Model):
+    name = models.CharField(primary_key=True, max_length=255)
+    exercises = models.ManyToManyField('Exercise')
 
     def __str__(self):
-        return self.exercise_name
-
-# class Perform(models.Model):
-#     score = models.IntegerField(null=False)
-#     date = models.DateTimeField()
-#     exercise = models.ForeignKey(Exercise)
-#     student = models.ForeignKey(Student)
-#
-#     def __str__(self):
-#         return "{0} {1} ({2}): {3}".format(self.student, self.exercise, self.date, self.score)
+        return self.group_name
 
 
-# class Teach(models.Model):
-#     teacher = models.ForeignKey(Teacher)
-#     group = models.ForeignKey(Group)
-#
-#     def __str__(self):
-#         return "{0} {1}".format(self.teacher, self.group)
+class Activity(models.Model):  # Une activité est composée de plusieurs exercices
+    name = models.CharField(primary_key=True, max_length=255)
+    date = models.DateTimeField()
+    multi_attempts = models.BooleanField()  # tentatives multiples ?
+    interactive_correction = models.BooleanField()  # correction interactive ? utile en materelle
+
+    teacher = models.ForeignKey('Teacher')
+
+    def __str__(self):
+        return self.name
 
 
-# class Destined(models.Model):
-#     exercise = models.ForeignKey(Exercise)
-#     group = models.ForeignKey(Group)
-#
-#     def __str__(self):
-#         return "{0} {1}".format(self.exercise, self.group)
+class Exercise(models.Model):
+    exercise_json = models.CharField(max_length=16384)#TODO
+    activity = models.ForeignKey('Activity')
+
+
+class CorrectionElement(models.Model):
+    index = models.IntegerField()
+    content = models.CharField(max_length=255)
+    exercise = models.ForeignKey('Exercise')
+
+
+class Reply(models.Model):
+    date = models.DateTimeField()
+
+    exercise = models.ForeignKey('Exercise')
+    student = models.ForeignKey('Student')
+
+    index = models.IntegerField()
+    content = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "{0} {1} ({2})".format(self.student, self.exercise, self.date)
